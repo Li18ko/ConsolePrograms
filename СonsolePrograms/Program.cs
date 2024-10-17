74c82a383ab1f29a7a8c455d765652f83822bb1b
Li18ko <lizakorneeva2004@gmail.com>
2024-10-17 12:41:08 +0300

    Создание репозитория

---
+++ СonsolePrograms/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace СonsolePrograms {
+    internal class Programs{
+        public static void Main(string[] args){
+           Console.WriteLine("Hello World!");
+           
+        }
+    }
+}
