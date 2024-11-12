2d9b0bddfe570893a0112c34da337032cc1c7c82
lizak <lizakorneeva2004@gmail.com>
2024-11-13 02:29:03 +0300

    Обработка ошибки + вызов исключения

--- СonsolePrograms/Program.cs
+++ СonsolePrograms/Program.cs
@@ -28,6 +28,11 @@ namespace СonsolePrograms {
             log.Info("Начало");
             
             string token = GitHubHelpers.GetInput("Введите токен: ", log);
+            
+            if (string.IsNullOrEmpty(token)) {
+                log.Error("Токен не был введен.");
+                throw new ArgumentException("Токен не был введен");
+            }
 
             try {
                 log.Debug("Создание экземпляра GitHubClient");
@@ -37,49 +42,50 @@ namespace СonsolePrograms {
                 var repositories = await gitHubClient.GetRepositories();
                 log.Debug("Http запрос успешен, список репозиториев получен");
                 
-                foreach (var repo in repositories)
-                {
+                foreach (var repo in repositories){
                     GitHubHelpers.PrintRepositoryInfo(repo);
                 }
 
                 string repositoryName = GitHubHelpers.GetInput("Введите имя репозитория: ", log);
+                if (string.IsNullOrEmpty(repositoryName)) {
+                    log.Error("Имя репозитория не было введено");
+                    throw new ArgumentException("Имя репозитория не было введено");
+                }
                     
-                try {
-                    log.Debug($"Поиск репозитория '{repositoryName}'");
-                    var selectedRepo = repositories.FirstOrDefault(repo =>
-                        repo.Name.Equals(repositoryName, StringComparison.OrdinalIgnoreCase));
-                    
-                    log.Debug($"Репозиторий существует: {selectedRepo.Name}");
+                log.Debug($"Поиск репозитория '{repositoryName}'");
+                var selectedRepo = repositories.FirstOrDefault(repo =>
+                    repo.Name.Equals(repositoryName, StringComparison.OrdinalIgnoreCase));
+                
+                log.Debug($"Репозиторий существует: {selectedRepo.Name}");
 
-                    log.Debug($"Запрашивание списка коммитов репозитория '{repositoryName}'");
-                    var commits = await gitHubClient.GetCommits(selectedRepo.Owner.Login, selectedRepo.Name);
-                    log.Debug("Коммиты найдены");
-                    foreach (var commit in commits) {
-                        GitHubHelpers.PrintCommitInfo(commit);
-                    }
-                }
-                catch (HttpRequestException ex) {
-                    log.Error("Ошибка при HTTP-запросе: " + ex);
-                }
-                catch (NullReferenceException ex) {
-                    log.Error($"Репозитория с именем '{repositoryName}' не существует, " + ex);
-                }
-                catch (Exception ex) {
-                    log.Error("Произошла непредвиденная ошибка: " + ex);
+                log.Debug($"Запрашивание списка коммитов репозитория '{repositoryName}'");
+                var commits = await gitHubClient.GetCommits(selectedRepo.Owner.Login, selectedRepo.Name);
+                log.Debug("Коммиты найдены");
+                foreach (var commit in commits) {
+                    GitHubHelpers.PrintCommitInfo(commit);
                 }
                 
             }
             catch (HttpRequestException ex) when (ex.Message.Contains("401 (Unauthorized)")) {
-                log.Error("Ошибка 401: Неверный или недействительный токен, " + ex);
+                log.Error("Ошибка 401: Неверный или недействительный токен, " + ex.Message);
+                throw new UnauthorizedAccessException("Ошибка 401: Неверный или недействительный токен");
+            }
+            catch (HttpRequestException ex) {
+                log.Error("Ошибка при HTTP-запросе: " + ex.Message);
+                throw new HttpRequestException("Ошибка при HTTP-запросе");
+            }
+            catch (NullReferenceException ex) {
+                log.Error($"Репозитория с таким именем не существует, " + ex.Message);
+                throw new NullReferenceException($"Репозитория с таким именем не существует");
             }
             catch (Exception ex) {
-                log.Error("Произошла непредвиденная ошибка: " + ex);
+                log.Error("Произошла непредвиденная ошибка: " + ex.Message);
+                throw new Exception("Произошла непредвиденная ошибка");
             }
             finally {
                 log.Info("Программа завершена");
             }
         }
-
     }
 }
 
