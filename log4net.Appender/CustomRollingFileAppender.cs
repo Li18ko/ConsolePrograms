2fdeaffcf0133d00e006ec4c8186e0acac27fff4
lizak <lizakorneeva2004@gmail.com>
2024-12-04 11:03:19 +0300

    Добавление кастомной библиотеки в локальный NuGet

---
+++ log4net.Appender/CustomRollingFileAppender.cs
@@ -0,0 +1,64 @@
+﻿using log4net.Appender;
+using System.IO.Compression;
+
+namespace ConsolePrograms {
+    public class CustomRollingFileAppender: RollingFileAppender {
+        
+        public int MaxFileInDirectories { get; set; }
+        public int MaxFileInArchive { get; set; }
+        public string? LocationLog { get; set; }
+        public string? LocationLogArchive { get; set; }
+        
+        public override void ActivateOptions() {
+            base.ActivateOptions();
+
+            Console.WriteLine($"MaxFileInDirectories: {MaxFileInDirectories}");
+            Console.WriteLine($"MaxFileInArhiv: {MaxFileInArchive}");
+            Console.WriteLine($"LocationLog: {LocationLog}");
+            Console.WriteLine($"LocationLogArchive: {LocationLogArchive}");
+        }
+        
+        protected override void AdjustFileBeforeAppend() {
+            ArchiveLogs(LocationLog, LocationLogArchive);
+            base.AdjustFileBeforeAppend();
+        }
+
+        private void ArchiveLogs(string? sourceDirectory, string? zipPath) {
+            if (sourceDirectory != null) {
+                IEnumerable<string> files = Directory.EnumerateFiles(sourceDirectory, "*.log");
+                IEnumerable<string> logFiles = files.Where(file => {
+                    DateTime fileLastWriteTime = System.IO.File.GetLastWriteTime(file);
+                    DateTime currentDate = DateTime.Now.Date;
+                    DateTime fileDate = fileLastWriteTime.Date;
+                    return (currentDate - fileDate).Days >= MaxFileInDirectories;
+                });
+
+                if (zipPath != null)
+                    using (ZipArchive zip = ZipFile.Open(zipPath, ZipArchiveMode.Update)) {
+                        foreach (string logFile in logFiles) {
+                            string entryName = Path.Combine("archive", Path.GetFileName(logFile));
+                            zip.CreateEntryFromFile(logFile, entryName, CompressionLevel.Optimal);
+                            System.IO.File.Delete(logFile);
+                        }
+
+                        LimitLogFilesInArchive(zip);
+                    }
+            }
+        }
+        
+        private void LimitLogFilesInArchive(ZipArchive zip){
+            List<ZipArchiveEntry> logEntries = zip.Entries
+                .Where(entry => entry.FullName.StartsWith("archive\\") && entry.Name.EndsWith(".log"))
+                .OrderBy(entry => entry.LastWriteTime)  
+                .ToList();
+            
+
+            while (logEntries.Count > MaxFileInArchive) {
+                ZipArchiveEntry entryToDelete = logEntries.First();
+                entryToDelete.Delete();
+                logEntries.RemoveAt(0); 
+            }
+        }
+        
+    }
+}
